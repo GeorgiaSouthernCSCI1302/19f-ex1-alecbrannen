f32a8a52ba10cd5a473b89cb8fb8ce55f9f28682
jb25254 <jb25254@CEIT2553214X002.ad.georgiasouthern.edu>
2019-08-21 16:50:14 -0400

    Added a prompt for last name.

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,10 +1,17 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello my name is Alec!");
 		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your last name? ");
+		String name = input.next();
+		input.close();
 		//add a statement to display your name in the console
 	
 		//Then commit your changes
